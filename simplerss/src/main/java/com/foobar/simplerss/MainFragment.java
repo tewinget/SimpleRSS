ab619a0bb8ed56439f452ef87df9e45f9171df60
Thomas Winget <tewinget@gmail.com>
2014-05-21 20:04:25 -0400

    Replaced Main Placeholder Fragment

---
+++ simplerss/src/main/java/com/foobar/simplerss/MainFragment.java
@@ -0,0 +1,66 @@
+package com.foobar.simplerss;
+
+/**
+ * Created by tom on 5/21/14.
+ */
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainFragment extends Fragment
+{
+    /**
+     * The fragment argument representing the section number for this
+     * fragment.
+     */
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    /**
+     * Returns a new instance of this fragment for the given section
+     * number.
+     */
+    public static MainFragment newInstance(int sectionNumber) {
+        MainFragment fragment = new MainFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public MainFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        ListView listView = (ListView) rootView.findViewById(R.id.entriesListView);
+        listView.setAdapter(new ArrayAdapter<String>(
+                getActivity(),
+                R.layout.feed_entry,
+                R.id.feedEntryText,
+                new String[]{
+                        "Item 1",
+                        "Item 2",
+                        "Item 3",
+                }));
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        ((MainActivity) activity).onSectionAttached(
+                getArguments().getInt(ARG_SECTION_NUMBER));
+    }
+}
